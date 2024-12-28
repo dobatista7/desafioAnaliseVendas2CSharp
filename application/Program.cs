5d9f74931db7cf4ca4831f7089889ff323230b38
Douglas Batista <dobatista7@gmail.com>
2024-12-28 09:04:43 -0300

    configuracao de separador decimal e entrada do caminho do arquivo

--- application/Program.cs
+++ application/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace DesafioAnaliseVendas2CSharp
+﻿using System.Globalization;
+
+namespace DesafioAnaliseVendas2CSharp.application
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Configuração para usar ponto como separador decimal
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+
+            Console.Write("Entre o caminho do arquivo: ");
+            string path = Console.ReadLine(); // Leitura do caminho do arquivo pelo usuário
+            Console.WriteLine();
         }
     }
 }
